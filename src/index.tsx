dfa7381c5a09e5149cfb6870ef44994f61a78c5d
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2018-03-19 17:15:44 -0400

    feat: file directory reestructuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './styles/global.css';
 
 ReactDOM.render(
   <App />,
