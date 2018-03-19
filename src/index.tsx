3b6ec17ecf5d898e2661fa79f3f76009f0c840e1
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2018-03-19 17:26:39 -0400

    feat: material ui installed and initilized with the roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/global.css';
 
+const MaterialWrapper = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <App />,
+  <MaterialWrapper />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
