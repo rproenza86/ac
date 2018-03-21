0bc3e380dedaab7b15fd3e702981078e54ca67dc
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2018-03-20 20:48:08 -0400

    feat: firebase code config inclusion, commands config and css defer work started

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './components/app/App';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
 import './styles/global.css';
 
 import { createStore, compose } from 'redux';
@@ -33,3 +34,12 @@ ReactDOM.render(
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
+
+firebase.initializeApp({
+  'apiKey': 'AIzaSyBFYpHBN-TmD-TUxQtVXgSFS7s7soUwnbA',
+  'databaseURL': 'https://atomic-coders.firebaseio.com',
+  'storageBucket': 'atomic-coders.appspot.com',
+  'authDomain': 'atomic-coders.firebaseapp.com',
+  'messagingSenderId': '396010841523',
+  'projectId': 'atomic-coders'
+});
